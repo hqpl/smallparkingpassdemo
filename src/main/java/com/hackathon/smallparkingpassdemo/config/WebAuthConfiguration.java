5ef21b2f333558ece67e4a55429c0088082c26f0
hqpl <dabroszmateusz@gmail.com>
2025-03-19 20:25:07 +0100

    Added CORS to allow frontent connection.

--- src/main/java/com/hackathon/smallparkingpassdemo/config/WebAuthConfiguration.java
+++ src/main/java/com/hackathon/smallparkingpassdemo/config/WebAuthConfiguration.java
@@ -1,5 +1,7 @@
 package com.hackathon.smallparkingpassdemo.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,6 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class WebAuthConfiguration {
@@ -16,7 +21,7 @@ public class WebAuthConfiguration {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.httpBasic(Customizer.withDefaults());
-
+        http.cors(cors ->cors.configurationSource(corsConfigurationSource()));
         http.csrf(AbstractHttpConfigurer::disable);
 
 
@@ -36,4 +41,16 @@ public class WebAuthConfiguration {
 
         return http.build();
     }    
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://192.168.0.3:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("X-HACKATHON", "Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
